8f1e6019e5fd1d403f0269b78c8368085ac1bc60
HyunSang Park <me@hyunsang.dev>
2023-09-19 16:49:52 +0900

    20230919: 를 추가하여 변수로 지정함.

---
+++ ch03/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/park.hyunsang/dev/github/The_Rust_Programming_Language/ch03/variables/target/debug/variables: /Users/park.hyunsang/dev/github/The_Rust_Programming_Language/ch03/variables/src/main.rs
